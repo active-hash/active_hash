747fa5f7f74b0e1b1d944ea6406a524ada87cbf0
Jeff Dean <jeff@zilkey.com>
2009-10-13 01:38:58 -0400

    added mysterious to_ary and to_str methods that some version of rspec seem to barf on

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,14 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'active_hash'
 
+ActiveHash::Base.class_eval do
+  def self.to_ary
+  end
+
+  def self.to_str
+  end
+end
+
 Spec::Runner.configure do |config|
   config.include ActsAsFu
   config.include Fixjour
