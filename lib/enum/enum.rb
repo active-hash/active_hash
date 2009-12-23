a518b24feee90c09f14a0f9b2769322a1584b8db
John Pignata <john.pignata@gmail.com>
2009-12-23 13:10:42 -0500

    Handle .delete_all and .insert methods on ActiveHash and descendants

--- lib/enum/enum.rb
+++ lib/enum/enum.rb
@@ -1,7 +1,7 @@
 module ActiveHash
   module Enum
 
-    DuplicateConstant = Class.new(RuntimeError)
+    DuplicateEnumAccessor = Class.new(RuntimeError)
 
     def enum_accessor(field_name)
       @enum_accessor = field_name
@@ -10,30 +10,36 @@ module ActiveHash
 
     def insert(record)
       super
-      set_constant(record) if enum_accessor?
+      set_constant(record) if @enum_accessor.present?
     end
 
-    private :insert
+    def delete_all
+      if @enum_accessor.present?
+        @records.each do |record|
+          constant = constant_for(record.attributes[@enum_accessor])
+          remove_const(constant) if const_defined?(constant)
+        end
+      end
+      super
+    end
 
     def set_constant(record)
-      if constant = constant_for(record.attributes[@enum_accessor])
-        self.const_set(constant, record)
+      constant = constant_for(record.attributes[@enum_accessor])
+      return nil if constant.blank?
+
+      unless const_defined?(constant)
+        const_set(constant, record)
+      else
+        raise DuplicateEnumAccessor, "#{constant} already defined for #{self.class}" unless const_get(constant) == record
       end
     end
 
     private :set_constant
 
-    def enum_accessor?
-      !@enum_accessor.nil?
-    end
-
-    private :enum_accessor?
-
     def constant_for(field_value)
-      if constant = field_value.dup
+      if constant = field_value.try(:dup)
         constant.gsub!(/[^A-Za-z]*/, "") 
         constant.upcase!
-        raise DuplicateConstant, "#{constant} is already defined on #{self.class.name}" if const_defined?(constant)            
         constant
       end
     end
--- spec/enum/enum_spec.rb
+++ spec/enum/enum_spec.rb
@@ -8,6 +8,7 @@ describe ActiveHash::Base, "enum" do
     class Borough < ActiveYaml::Base
       extend ActiveHash::Enum
       fields :name, :county, :population
+      enum_accessor :name
     end
   end
 
@@ -17,10 +18,9 @@ describe ActiveHash::Base, "enum" do
 
   describe "#enum_accessor" do
     it "sets the field used for accessing records by constants" do
-      Borough.enum_accessor :name
       Borough::BROOKLYN.should == Borough.find_by_name("Brooklyn")
     end
-    
+
     it "ensures that values stored in the field specified are unique" do
       lambda do
         Class.new(ActiveHash::Base) do
@@ -32,7 +32,7 @@ describe ActiveHash::Base, "enum" do
           ]
           enum_accessor :name 
         end
-      end.should raise_error(ActiveHash::Enum::DuplicateConstant)
+      end.should raise_error(ActiveHash::Enum::DuplicateEnumAccessor)
     end
 
     it "removes non-word characters from values before setting constants" do
@@ -52,4 +52,35 @@ describe ActiveHash::Base, "enum" do
     end
   end
 
+  context "ActiveHash with an enum_accessor set" do
+    describe "#save" do
+      it "resets the constant's value to the updated record" do
+        Borough::BROOKLYN.population.should == 2556598
+        brooklyn = Borough.find_by_name("Brooklyn")
+        brooklyn.population = 2556600
+        brooklyn.save.should be_true
+        Borough::BROOKLYN.population.should == 2556600
+      end
+    end
+
+    describe ".create" do
+      it "creates constants for new records" do
+        bronx = Borough.create!(:name => "Bronx")
+        Borough::BRONX.should == bronx
+      end
+
+      it "doesn't create constants for records missing the enum accessor field" do
+        Borough.create(:name => "").should be_true
+        Borough.create(:population => 12).should be_true
+      end
+    end
+
+    describe ".delete_all" do
+      it "unsets all constants for deleted records" do
+        Borough.const_defined?("STATENISLAND").should be_true
+        Borough.delete_all.should be_true
+        Borough.const_defined?("STATENISLAND").should be_false
+      end
+    end
+  end
 end
