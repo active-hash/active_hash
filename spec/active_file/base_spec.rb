a037beba885fa463645042d83f4cb604a4c18bd8
Jeff Dean <jeff@zilkey.com>
2010-12-06 00:19:49 -0700

    make spec helper requires more concise

--- spec/active_file/base_spec.rb
+++ spec/active_file/base_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe ActiveFile::Base do
   before do
--- spec/enum/enum_spec.rb
+++ spec/enum/enum_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe ActiveHash::Base, "enum" do
 
