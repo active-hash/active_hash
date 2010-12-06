ef5600b28a859586a2b4561e0b65a192be4f40d5
Jeff Dean <jeff@zilkey.com>
2010-12-06 00:22:26 -0700

    update specs to use rspec 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'rspec/autorun'
 require 'active_record'
 require 'fixjour'
 
@@ -7,6 +7,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'active_hash'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.include Fixjour
 end
