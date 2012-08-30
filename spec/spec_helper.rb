fcda10eed68536646f4b04bfcd11899a862f9fb6
Gilles Deleuze <gilles.deleuze@papercavalier.com>
2012-08-30 11:54:45 +0000

    Run specs in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,7 @@ require 'rspec/autorun'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'active_hash'
+
+RSpec.configure do |c|
+  c.order = 'random'
+end
