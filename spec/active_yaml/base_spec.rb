f71d07fecef29cde5a1c0e2c7f3434ef924fb701
Matthew O'Riordan <matthew.oriordan@gmail.com>
2013-07-05 23:12:20 +0100

    Preload the schema when finder methods called on ActiveHash
    This commit allows you to call #find_by_attribute on an ActiveHash without having to explicitly call #all first.

--- spec/active_yaml/base_spec.rb
+++ spec/active_yaml/base_spec.rb
@@ -27,6 +27,15 @@ describe ActiveYaml::Base do
     end
   end
 
+  describe ".where" do
+    context "before the file is loaded" do
+      it "reads from the file and filters by where statement" do
+        State.where(:name => 'Oregon').should_not be_empty
+        State.count.should > 0
+      end
+    end
+  end
+
   describe ".delete_all" do
     context "when called before .all" do
       it "causes all to not load data" do
@@ -91,6 +100,18 @@ describe ActiveYaml::Base do
 
     it 'returns a single city based on find_by_id' do
       City.find_by_id(1).name.should == 'Albany'
+    end
+
+  end
+
+  describe 'meta programmed finders and properties for fields that exist in the YAML' do
+
+    it 'should have a finder method for each property' do
+      City.find_by_state('Oregon').should_not be_nil
+    end
+
+    it 'should have a find all method for each property' do
+      City.find_all_by_state('Oregon').should_not be_nil
     end
 
   end
