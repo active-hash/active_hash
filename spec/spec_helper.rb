f1848a90fc4631a53a86e613cd2b4750f76a99c8
aloha <y.alohac@gmail.com>
2017-02-24 21:43:22 +0800

    feat/thread_safe make sure in multithread env still working well

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,11 @@ require 'active_hash'
 Dir["spec/support/**/*.rb"].each { |f|
   require File.expand_path(f)
 }
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+  config.before(:each) do
+    Thread.current[:data_loaded] = nil
+  end
+end
