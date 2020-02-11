dc67774a3f30a3b051446f43fc722b3d602af777
Yoshiyuki Hirano <yhirano@me.com>
2020-02-11 19:16:44 +0900

    Testable per file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ SKIP_ACTIVE_RECORD = ENV['SKIP_ACTIVE_RECORD']
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'active_hash'
+require 'active_record' unless SKIP_ACTIVE_RECORD
 
 Dir["spec/support/**/*.rb"].each { |f|
   require File.expand_path(f)
