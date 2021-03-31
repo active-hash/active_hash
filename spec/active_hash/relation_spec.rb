9e0e4d5368bb096c81fa2685b50552051302e312
sluceno <sluceno.a@gmail.com>
2021-03-31 12:50:53 +0200

    Add ActiveHash::Relation#size method for compatibily
    With Rails 6.1.3.1 ActionView::Renderer::CollectionRenderer calls size
    method over collections. Adding the size method makes ActiveHash
    compatible with it

--- spec/active_hash/relation_spec.rb
+++ spec/active_hash/relation_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ActiveHash::Relation do
       ]
     end
   end
-  
+
   subject { model_class.all }
 
   describe '#sample' do
@@ -24,18 +24,30 @@ RSpec.describe ActiveHash::Relation do
       expect(records.sample(2).count).to eq(2)
     end
   end
-  
+
   describe '#to_ary' do
     it 'returns an array' do
       expect(subject.to_ary).to be_an(Array)
     end
-    
+
     it 'contains the same items as the relation' do
       array = subject.to_ary
-      
+
       expect(array.length).to eq(subject.count)
       expect(array.first.id).to eq(1)
       expect(array.second.id).to eq(2)
+    end
+  end
+
+  describe '#size' do
+    it 'returns an Integer' do
+      expect(subject.size).to be_an(Integer)
+    end
+
+    it 'returns the correct number of items of the relation' do
+      array = subject.to_ary
+
+      expect(array.size).to eq(2)
     end
   end
 end
