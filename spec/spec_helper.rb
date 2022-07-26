011cf977a8b105a1d4402be3eee23c80f3f69494
machisuke <d.backford1231@gmail.com>
2022-07-26 09:09:44 +0900

    isolate tests with temporary classes
    ref:
    https://groups.google.com/g/rspec/c/7CQq0ABS3yQ

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,13 @@ require 'active_record' unless SKIP_ACTIVE_RECORD
 Dir["spec/support/**/*.rb"].each { |f|
   require File.expand_path(f)
 }
+
+RSpec.configure do |config|
+  config.after(:each) do
+    # To isolate tests with temporary classes.
+    # ref: https://groups.google.com/g/rspec/c/7CQq0ABS3yQ
+    if ActiveRecord::VERSION::MAJOR < 7
+      ActiveSupport::Dependencies::Reference.clear!
+    end
+  end
+end
