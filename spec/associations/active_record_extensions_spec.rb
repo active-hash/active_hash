95d8ad538aa3330111add1c5f37a8bd7cc57d7f8
machisuke <d.backford1231@gmail.com>
2022-07-30 11:21:37 +0900

    use where in scope because it is more common use case

--- spec/associations/active_record_extensions_spec.rb
+++ spec/associations/active_record_extensions_spec.rb
@@ -151,17 +151,22 @@ unless SKIP_ACTIVE_RECORD
 
         if ActiveRecord::VERSION::MAJOR > 3
           it "doesn't interfere with AR's procs in belongs_to methods" do
-            School.belongs_to :country, lambda { select(:id) }
+            School.belongs_to :country, lambda { where(name: 'Japan') }
             school = School.new
             country = Country.create!(id: 1, name: 'Japan')
             school.country = country
             expect(school.country).to eq(country)
             expect(school.country_id).to eq(country.id)
-            expect(school.country.attributes).to eq({ 'id' => 1, 'name' => 'Japan' })
+            expect(school.country).to eq(country)
             school.save!
             school.reload
             expect(school.country_id).to eq(country.id)
-            expect(school.country.attributes).to eq({ 'id' => 1 })
+            expect(school.country).to eq(country)
+
+            country.update!(name: 'JAPAN')
+            school.reload
+            expect(school.country_id).to eq(country.id)
+            expect(school.country).to eq(nil)
           end
         end
 
